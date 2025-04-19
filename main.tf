83cf6a808247eb3fa4f3ebdf520f0989baa5dc27
Rex Makusia <rmakusia@alumni.dwu.ac.pg>
2025-04-19 17:21:46 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ resource "azurerm_resource_group" "mtc-rg" {
   }
 }
 
-resource "azurerm_virtual" "mtc-vn" {
+resource "azurerm_virtual_network" "mtc-vn" {
 name = "mtc-network"
 resource_group_name = azurerm_resource_group.mtc-rg.name
 location = azurerm_resource_group.mtc-rg.location
