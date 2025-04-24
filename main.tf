6e5067934bfcc00fcb008f810042e97da084f573
Rex Makusia <rmakusia@alumni.dwu.ac.pg>
2025-04-24 16:20:42 -0700

    Update the security group association

--- main.tf
+++ main.tf
@@ -60,3 +60,8 @@ resource "azurerm_network_security_rule" "mtc-dev-rule" {
   resource_group_name         = azurerm_resource_group.mtc-rg.name
   network_security_group_name = azurerm_network_security_group.mtc-sg.name
 }
+
+resource "azurerm_subnet_network_security_group_association" "mtc-sga" {
+  subnet_id                 = azurerm_subnet.mtc-subnet.id
+  network_security_group_id = azurerm_network_security_group.mtc-sg.id
+}
