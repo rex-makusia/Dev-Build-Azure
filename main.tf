ee54e658a649f52c791e10699cd8fda71aadd7fd
Rex Makusia <rmakusia@alumni.dwu.ac.pg>
2025-04-24 15:27:57 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -37,12 +37,12 @@ resource "azurerm_subnet" "mtc-subnet" {
   address_prefixes = ["10.123.1.0/24"]
 }
 
-resource "azure_network_security_group "mtc-sg" {
-  name = "mtc-sg"
-  location = azurerm_resource_group.mtc-rg.location
+resource "azurerm_network_security_group" "mtc-sg" {
+  name                = "mtc-sg"
+  location            = azurerm_resource_group.mtc-rg.location
   resource_group_name = azurerm_resource_group.mtc-rg.name
-  
-tags = {
-environment = "dev"
-}
+
+  tags = {
+    environment = "dev"
+  }
 }
