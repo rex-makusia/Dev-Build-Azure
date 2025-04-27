c65d0410982e4dba0d8dfc4a67437f1f61990a11
Rex Makusia <rmakusia@alumni.dwu.ac.pg>
2025-04-26 20:29:41 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,26 +15,26 @@ resource "azurerm_resource_group" "mtc-rg" {
   name     = "mtc-resources"
   location = "Australia East"
   tags = {
-      environment = "dev"
+    environment = "dev"
   }
 }
 
 resource "azurerm_virtual_network" "mtc-vn" {
-name = "mtc-network"
-resource_group_name = azurerm_resource_group.mtc-rg.name
-location = azurerm_resource_group.mtc-rg.location
-address_space = ["10.123.0.0/16"]
+  name                = "mtc-network"
+  resource_group_name = azurerm_resource_group.mtc-rg.name
+  location            = azurerm_resource_group.mtc-rg.location
+  address_space       = ["10.123.0.0/16"]
 
-tags = {
-environment = "dev"
-}
+  tags = {
+    environment = "dev"
+  }
 }
 
 resource "azurerm_subnet" "mtc-subnet" {
-  name = "mtc-subnet"
-  resource_group_name = azurerm_resource_group.mtc-rg.name
+  name                 = "mtc-subnet"
+  resource_group_name  = azurerm_resource_group.mtc-rg.name
   virtual_network_name = azurerm_virtual_network.mtc-vn.name
-  address_prefixes = ["10.123.1.0/24"]
+  address_prefixes     = ["10.123.1.0/24"]
 }
 
 resource "azurerm_network_security_group" "mtc-sg" {
@@ -78,30 +78,48 @@ resource "azurerm_public_ip" "mtc-ip" {
 }
 
 resource "azurerm_network_interface" "mtc-nic" {
-name = "mtc-nic"
-location = azurerm_resource_group.mtc-rg.location
-resource_group_name = azurerm_resource_group.mtc-rg.name
+  name                = "mtc-nic"
+  location            = azurerm_resource_group.mtc-rg.location
+  resource_group_name = azurerm_resource_group.mtc-rg.name
 
-ip_configuration  {
-  name = "internal"
-  subnet_id = azurerm_subnet.mtc-subnet.id
-  private_ip_address_allocation = "Dynamic"
-  public_ip_address_id = azurerm_public_ip.mtc-ip.id
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.mtc-subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.mtc-ip.id
 
-}
+  }
   tags = {
     environment = "dev"
   }
 }
 
-resource "azurerm_linux_virtual_machine" "mtc-vm"{
- name = "mtc-vm"
-resource_group_name = azurerm_resource_group.mtc-rg.name
-location = azurerm_resource_group.mtc-rg.location
-size = "Standard_B1s"
-admin_username = "adminuser"
-network_interface_ids = [azurerm_network_interface.mtc-nic.id] 
-tags = {
-  environment = "dev"
-}
+resource "azurerm_linux_virtual_machine" "mtc-vm" {
+  name                  = "mtc-vm"
+  resource_group_name   = azurerm_resource_group.mtc-rg.name
+  location              = azurerm_resource_group.mtc-rg.location
+  size                  = "Standard_B1s"
+  admin_username        = "adminuser"
+  network_interface_ids = [azurerm_network_interface.mtc-nic.id]
+
+ admin_ssh_key (
+  username  = "adminuser"
+  public_key = file("~/.ssh/id_rsa.pub")
+ )
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  tags = {
+    environment = "dev"
+  }
 }
