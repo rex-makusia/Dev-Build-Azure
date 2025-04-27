c610211c3be428644a07eee51dad218b18c1b8da
Rex Makusia <rmakusia@alumni.dwu.ac.pg>
2025-04-27 05:16:44 -0700

    Update the public key path

--- main.tf
+++ main.tf
@@ -104,7 +104,7 @@ resource "azurerm_linux_virtual_machine" "mtc-vm" {
 
  admin_ssh_key {
   username  = "adminuser"
-  public_key = file("~/.ssh/id_rsa.pub")
+  public_key = file("~/.ssh/id_mtc.pub")
  }
 
   os_disk {
