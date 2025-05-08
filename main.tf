65e0612f6fcf0a1f4b67d4aa9bf49c433cb326aa
Rex Makusia <rmakusia@alumni.dwu.ac.pg>
2025-05-08 16:24:59 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -134,3 +134,8 @@ tags = {
     environment = "dev"
   }
 }
+
+data "azurerm_public_ip" "mtc-ip-data" {
+name = azurerm_public_ip.mtc-ip.name
+resource_group_name = azurerm_resource_group.mtc-rg.name
+}
